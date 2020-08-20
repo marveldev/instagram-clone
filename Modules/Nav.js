bf5e8696424710e766aaababd0e05b4803fc7f82
Marvel Dev <marvellousalika48@gmail.com>
2020-08-20 16:50:43 +0100

    - add modules

---
+++ Modules/Nav.js
@@ -0,0 +1,15 @@
+const Nav = () => `
+  <nav class="nav">
+    <span class="logo">
+      <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt="logo">
+    </span>
+    <input type="text" class="search-input" placeholder="Search">
+    <span class="nav-links">
+      <a href="#"><i class="glyphicon glyphicon-home"></i></a>
+      <a href="#"><i class="fa fa-send"></i></a>
+      <a href="#"><i class='far fa-compass'></i></a>
+      <a href="#"><i class='far fa-heart'></i></a>
+    </span>
+  </nav>
+`
+export default Nav();
---
+++ Modules/profile.js
@@ -0,0 +1,14 @@
+const profile = () => {
+  return `
+    <section class="profile">
+      <a href="#" class="profile-photo">
+        <img src="https://images.pexels.com/photos/4864565/pexels-photo-4864565.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="">
+      </a>
+      <div class="profile-info">
+        <p>MarvelWonders</p>
+        <button>FOLLOW</button>
+      </div>
+    </section>
+  `
+}
+export default profile();
