9efeb1b84461f779976301031ef565c39d3bb754
Marvel Dev <marvellousalika48@gmail.com>
2020-10-20 19:35:22 +0100

    - add bio photo event listeners

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import Nav from './Modules/nav/Nav.js';
 import Profile from './Modules/profile/Profile.js';
 import Gallery from './Modules/gallery/Gallery.js';
 import { request } from './dataStorage.js';
-import  addBioEventListeners  from './Modules/profile/events.js';
+import  { addBioEventListeners, addBioPhotoEventListeners } from './Modules/profile/events.js';
 import { addGalleryEventListeners, addImagesToGallery } from './Modules/gallery/events.js';
 import GalleryModal from './Modules/gallery/galleryModal.js';
 
@@ -20,6 +20,7 @@ const app = async () => {
 
 request.onsuccess = async () => { 
   document.getElementById('root').innerHTML = await app();
+  addBioPhotoEventListeners();
   addBioEventListeners();
   addGalleryEventListeners();
   addImagesToGallery();
