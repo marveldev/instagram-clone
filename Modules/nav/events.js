3146fde7147b53306a5f6f996e68cdc1ef07a007
Marvel Dev <marvellousalika48@gmail.com>
2020-11-05 19:00:05 +0100

    - deleted

--- Modules/nav/events.js
+++
@@ -1,8 +0,0 @@
-const navEventListeners = () => {
-  const messageButton = document.querySelector('.message-btn');
-  messageButton.addEventListener('click', () => {
-    const message = document.querySelector('#message');
-    message.style.display = 'none';
-  })
-}
-export default navEventListeners;
